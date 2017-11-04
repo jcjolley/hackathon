35df90c5f061c4ccef10cf59df4f10927cccca6e
Cory Owens <cory.r.owens@gmail.com>
2017-11-04 15:09:54 -0600

    Improved responsiveness further.

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -2,12 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour {
+public class player : MonoBehaviour
+{
 
     public float verticalSpeed = 5;
     public float horizontalSpeed = 10;
-    private bool jumping = false;
-    private float lastHorizontal = 0;
     private Rigidbody2D rb2d;
 
     public void Start()
@@ -17,40 +16,32 @@ public class player : MonoBehaviour {
 
     public void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
         Vector2 velocity = rb2d.velocity;
-        if (System.Math.Abs(moveHorizontal) < System.Math.Abs(lastHorizontal))
+        if (moveHorizontal == 0)
         {
-            print("Stop");
             velocity.x = 0;
         }
         if (moveHorizontal > 0)
         {
             velocity.x = horizontalSpeed;
-        } else if (moveHorizontal < 0)
+        }
+        else if (moveHorizontal < 0)
         {
             velocity.x = -horizontalSpeed;
         }
-        lastHorizontal = moveHorizontal;
-        
-        if (moveVertical > 0)
+        if (onGround() && moveVertical > 0)
         {
-            if (!jumping)
-            {
-                velocity.y = verticalSpeed;
-                jumping = true;
-            }
+            velocity.y = verticalSpeed;
         }
-        if (rb2d.velocity.y == 0)
-        {
-            jumping = false;
-        }
+
         rb2d.velocity = velocity;
     }
-    
-    void FixedUpdate()
+
+    private bool onGround()
     {
-        
+        return rb2d.velocity.y == 0; // TODO: Replace with collision check. 
     }
+
 }
