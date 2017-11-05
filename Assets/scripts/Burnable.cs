50b011bdbaeed15002c481bf1b6cd3a4714905f9
Joshua Jolley <jolleyboy@gmail.com>
2017-11-04 19:50:07 -0600

    Watch it all burn

--- Assets/scripts/Burnable.cs
+++ Assets/scripts/Burnable.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Burnable : MonoBehaviour {
-
-    
+public class Burnable : MonoBehaviour
+{
+    public int burnThreshhold = 3;
+    bool burning = false;
+    float burningCount = 0;
     public void burn()
     {
         print("IT'S BURRRRRNING!");
-        Destroy(gameObject);
+        this.burning = true;
+    }
+
+    void Update()
+    {
+        if (this.burning)
+            this.burningCount += 1 * Time.deltaTime;
+        print(this.burningCount);
+        if (this.burningCount > burnThreshhold)
+        {
+            Destroy(gameObject);
+        }
     }
 }
